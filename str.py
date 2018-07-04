6764ecbb96c9caeff9cef84c71066f08cd764337
RekoLee <springworder@gmail.com>
2018-07-04 10:55:08 +0800

    string

---
+++ str.py
@@ -0,0 +1,15 @@
+#导入包 import os
+from os import *
+# 多行字符串 原样输出
+str='''how to 
+do ?'''
+print(str)
+# 字符串占位符
+msg='my score is %s points'
+score=95
+print(msg%score)
+# 字符串乘法
+print(' '*25+'hello')
+print(' '*25+'hello')
+print(' '*25+'hello')
+system("pause")
